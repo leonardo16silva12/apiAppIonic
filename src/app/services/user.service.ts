6470dc00a648788af39e5b9e26e4e069f47f8e36
Leonardo Silva <leonardo.silva1303@etec.sp.gov.br>
2020-10-21 12:42:39 -0300

    Aula 21-10

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private url = 'https://reqres.in/api/users'; 
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  public buscarTodos(pagina: number) {
+    if(pagina <= 0) {
+      pagina = 1;
+    }
+    return this.http.get(`${this.url}?page=${pagina}`);
+  }
+
+  public buscarId(id: number) {
+    return this.http.get(`${this.url}/${id}`);
+  }
+}
