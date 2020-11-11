51c7a3e2f80134b659102f523451fd7ff7391ca6
Leonardo Silva <leonardo.silva1303@etec.sp.gov.br>
2020-11-11 13:49:17 -0300

    Post, Put and Delete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { UserService } from '../services/user.service';
+import { Usuario } from '../models/Usuario';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +14,12 @@ export class HomePage {
   public listaUsuarios: any = [];
   public pagina = 1;
   public totalPaginas = 1;
+  public usuario: Usuario = {};
+  public id: number;
 
   constructor(
     private userService: UserService,
+    public alertController: AlertController,
   ) {}
 
   ionViewWillEnter() {
@@ -29,8 +35,37 @@ export class HomePage {
     this.userService.buscarTodos(pagina).subscribe(dados => {
       this.listaUsuarios = dados['data'];
       this.totalPaginas = dados['total_pages'];
-      console.log("Lista: ", this.listaUsuarios);
+      console.log(this.listaUsuarios);
+
     });
+  }
+
+  async showAlertDelete() {
+    const alert = await this.alertController.create({
+      header: 'Atenção',
+      message: 'Você tem certeza que deseja deletar o usuário?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        }, {
+          text: 'Ok',
+          handler: () => {
+            this.userService.deletar(this.usuario);
+          }
+        }
+      ]
+    });
+  
+    await alert.present();
+  }
+
+  public async deletar(id: number) {
+    console.log(this.usuario);
+    this.usuario.id = id;
+    console.log(this.usuario);
+    this.showAlertDelete();
+
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Usuario } from '../models/Usuario';
 
 @Injectable({
   providedIn: 'root'
@@ -22,4 +23,18 @@ export class UserService {
   public buscarId(id: number) {
     return this.http.get(`${this.url}/${id}`);
   }
+
+  public cadastrar(usuario: Usuario) {
+    return this.http.post(this.url, usuario);
+  }
+
+  public alterar(usuario: Usuario) {
+    return this.http.put(`${this.url}/${usuario.id}`, usuario);
+  }
+
+  public deletar(usuario: Usuario) {
+    console.log(this.url + '/' + usuario.id);
+    return this.http.delete(`${this.url}/${usuario.id}`);
+  }
+
 }
